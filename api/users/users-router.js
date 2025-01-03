6e32e37e495cb1d085a72f587f1d8bb362d4c9a2
Steve Schwab <schwabs@umich.edu>
2025-01-03 11:01:55 -0500

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
 const { restricted } = require('../auth/auth-middleware')
-const Users = require('./users-model')
+const User = require('./users-model')
 
 router.get('/', restricted, (req, res, next) => {
-  Users.find()
+  User.find()
     .then(users => {
       res.json(users)
     })
